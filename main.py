405f6c9ddabd306fe8cd76c3232ec256f1142b1e
DongJun <101547736+d5ngjun2@users.noreply.github.com>
2025-09-12 12:26:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
-import feedparser
-
-# RSS 피드 URL
-RSS_URL = "https://nikihwangg.tistory.com/rss"
-MAX_POSTS = 5
-
-# README 파일 경로
-README_FILE = "README.md"
-
-# RSS 파싱
-feed = feedparser.parse(RSS_URL)
-
-# 블로그 글 Markdown 생성
-blog_section = "\n### 📝 Latest Blog Posts\n"
-for entry in feed.entries[:MAX_POSTS]:
-    blog_section += f"- [{entry.title}]({entry.link})\n"
-
-# README 기존 내용 읽기
-with open(README_FILE, "r", encoding="utf-8") as f:
-    content = f.read()
-
-# "Latest Blog Posts" 섹션 이전 내용 삭제 후 새로 삽입
-import re
-new_content = re.sub(r'### 📝 Latest Blog Posts\n(.|\n)*', '', content)
-new_content += blog_section
-
-# README 업데이트
-with open(README_FILE, "w", encoding="utf-8") as f:
-    f.write(new_content)
+import feedparser, time
+ 
+URL = "https://nikihwangg.tistory.com/rss"
+RSS_FEED = feedparser.parse(URL)
+MAX_POST = 5
+ 
+markdown_text = """
+## ✅ Latest Blog Post
+ 
+"""  # list of blog posts will be appended here
+ 
+for idx, feed in enumerate(RSS_FEED['entries']):
+    if idx > MAX_POST:
+        break
+    else:
+        feed_date = feed['published_parsed']
+        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
+        
+f = open("README.md", mode="w", encoding="utf-8")
+f.write(markdown_text)
+f.close()
